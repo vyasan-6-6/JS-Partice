3d717524f2c344bb0756cf367fa8855e2df3c1d8
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-06-19 11:58:46 +0530

    feat : insertion sorting practice

--- javascript.js
+++ javascript.js
@@ -281,41 +281,79 @@
 //     return arr;
 // }
 
+// ------------bubblesort------------
 // console.log(bubbleSort(arr));
 
 // let arr = [1,10000, 3, 5, 9, 8];
 
 // function bubbleSort(arr) {
-//     const n = arr.length;
-//     for (let i = 0; i < n - 1; i++) {
-//         for (let j = 0; j < n - 1 - i; j++) {
-//             if (arr[j] > arr[j + 1]) {
- 
-//         [arr[j],arr[j+1]] = [arr[j+1],arr[j]]   
-       
-//             }
-//         }
-//     }
-//     return arr;
-// }
-
-// console.log(bubbleSort(arr));
-
-// let arr = [1,10000, 3, 5, 9, 8];
-
-// function bubbleSortReverse(arr) {
-//     const n = arr.length;
-//     for (let i = n - 1; i > 0 ; i--) {
-//         for (let j =  n - 1 - i; j > 0; j--) {
-//             if (arr[j] > arr[j - 1]) {
- 
-//         [arr[j],arr[j-1]] = [arr[j-1],arr[j]]   
-       
-//             }
-//         }
-//     }
+    //     const n = arr.length;
+    //     for (let i = 0; i < n - 1; i++) {
+        //         for (let j = 0; j < n - 1 - i; j++) {
+            //             if (arr[j] > arr[j + 1]) {
+                
+            //         [arr[j],arr[j+1]] = [arr[j+1],arr[j]]   
+            
+            //             }
+            //         }
+            //     }
+            //     return arr;
+            // }
+            
+            // console.log(bubbleSort(arr));
+            
+            // let arr = [1,10000, 3, 5, 9, 8];
+            
+            // function bubbleSortReverse(arr) {
+                //     const n = arr.length;
+                //     for (let i = n - 1; i > 0 ; i--) {
+                    //         for (let j =  n - 1 - i; j > 0; j--) {
+                        //             if (arr[j] > arr[j - 1]) {
+                            
+                        //         [arr[j],arr[j-1]] = [arr[j-1],arr[j]]   
+                        
+                        //             }
+                        //         }
+                        //     }
 //     return arr;
 // }
 
 // console.log(bubbleSortReverse(arr));
 
+// function sortAscending(arr) {
+    //   for (let i = 0; i < arr.length; i++) {
+        //     for (let j = i + 1; j < arr.length; j++) {
+            //       if (arr[i] > arr[j]) {
+                //         [arr[i], arr[j]] = [arr[j], arr[i]];
+                //       }
+                //     }
+                //   }
+                
+                //   return arr;
+                // }
+                
+                // console.log(sortAscending([1,4,3,5,2]));
+                
+                // ------------insertion sort------------
+
+
+                function insertionSort(arr) {
+    let n = arr.length;
+
+    for (let i = 1; i < n; i++) {
+
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = current;
+    }
+
+    return arr;
+}
+
+console.log(insertionSort([5, 3, 4, 1]));
