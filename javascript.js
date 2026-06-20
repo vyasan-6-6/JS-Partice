631165fd0f9636346264192560b26a2df3e42f9f
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-06-20 11:26:49 +0530

    feat : Binary search practice

--- javascript.js
+++ javascript.js
@@ -359,12 +359,31 @@
 
 // ------------Linear Search------------
 
-function linearSearch(arr,tar) {
-for(let i = 0 ; i < arr.length ; i++){
-    if(arr[i]===tar){
-    return i;
+// function linearSearch(arr,tar) {
+// for(let i = 0 ; i < arr.length ; i++){
+//     if(arr[i]===tar){
+//     return i;
+//     }
+// }
+// return -1
+// }
+// console.log(linearSearch([4,8,2,7,5,9],7));
+
+// ------------binarySearch  ------------
+function binarySearch(arr, tar) {
+    let left   = 0;
+    let right = arr.length - 1;
+    while(left   <= right) {
+        let mid = Math.floor((left+right)/2);
+        if(arr[mid]===tar){
+            return mid;
+        }
+        if(tar<arr[mid]){
+            right= mid - 1;
+        }else{
+            left = mid + 1;
+        }
     }
+    return -1;
 }
-return -1
-}
-console.log(linearSearch([4,8,2,7,5,9],7));
+console.log(binarySearch([1, 3, 5, 7, 9, 11],7));
