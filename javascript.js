70b5d81bd8357a453aeda8f31c3595f350f2c9e4
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-06-23 17:23:25 +0530

    feat :optimized  prime  number checking

--- javascript.js
+++ javascript.js
@@ -651,9 +651,24 @@
 
 //----prime number-----
 
-function isPrime(n){
+// function isPrime(n){
+//     if(n<2) return false;
+//     for(let i =2;i < n;i++){
+//         if(n%i===0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log(isPrime(0));
+// console.log(isPrime(1));
+// console.log(isPrime(4));
+// console.log(isPrime(5));
+// console.log(isPrime(7));
+
+function isPrime(n){//O(sqrt(n))
     if(n<2) return false;
-    for(let i =2;i < n;i++){
+    for(let i =2;i <=Math.sqrt(n);i++){
         if(n%i===0){
             return false;
         }
@@ -661,6 +676,7 @@ function isPrime(n){
     return true;
 }
 console.log(isPrime(0));
+console.log(isPrime(2));
 console.log(isPrime(1));
 console.log(isPrime(4));
 console.log(isPrime(5));
