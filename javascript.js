3f562fc1bdf297998f748349d295d80b65c8f3a1
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-06-24 19:08:11 +0530

    feat : Quick  sort   logic implementation

--- javascript.js
+++ javascript.js
@@ -820,18 +820,40 @@
 // }
 // console.log(bubbleSort(arr));
 
-const arr = [4,3,2,1];
-function insertionSort(arr){
-for(let i = 1 ; i < arr.length ; i++){
-    let current = arr[i];
-    let j = i-1;
-    while(j>=0 && arr[j] > current){
-        arr[j+1] =arr[j];
-        j--;
-    }
-    arr[j+1] = current;
+// const arr = [4,3,2,1];
+// function insertionSort(arr){
+// for(let i = 1 ; i < arr.length ; i++){
+//     let current = arr[i];
+//     let j = i-1;
+//     while(j>=0 && arr[j] > current){
+//         arr[j+1] =arr[j];
+//         j--;
+//     }
+//     arr[j+1] = current;
 
+// }
+// return arr;
+// }
+// console.log(insertionSort(arr));
+
+//---------------Quick sort-----------
+
+const arr = [4, 3, 2, 1];
+function quickSort(arr) {
+    if(arr.length < 2){
+        return arr;
+    }
+    let pivot = arr[arr.length - 1];
+    let right = [];
+    let left = [];
+    for (let i = 0; i < arr.length-1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return [...quickSort(left),pivot,...quickSort(right)]
 }
-return arr;
-}
-console.log(insertionSort(arr));
+console.log(quickSort(arr));
+
