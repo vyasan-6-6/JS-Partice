553de3322f0f39ba6ebecdee7c705de44c81b5cd
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-06-25 13:34:36 +0530

    feat : Merge  sort   logic implementation

--- javascript.js
+++ javascript.js
@@ -838,22 +838,47 @@
 
 //---------------Quick sort-----------
 
-const arr = [4, 3, 2, 1];
-function quickSort(arr) {
-    if(arr.length < 2){
-        return arr;
-    }
-    let pivot = arr[arr.length - 1];
-    let right = [];
-    let left = [];
-    for (let i = 0; i < arr.length-1; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
+// const arr = [4, 3, 2, 1];
+// function quickSort(arr) {
+//     if(arr.length <=1){
+//         return arr;
+//     }
+//     let pivot = arr[arr.length - 1];
+//     let right = [];
+//     let left = [];
+//     for (let i = 0; i < arr.length-1; i++) {
+//         if (arr[i] < pivot) {
+//             left.push(arr[i]);
+//         } else {
+//             right.push(arr[i]);
+//         }
+//     }
+//     return [...quickSort(left),pivot,...quickSort(right)]
+// }
+// console.log(quickSort(arr));
+
+const arr = [3, 5, 1, 5, 0, 2, 13, 6, 7, 88, 654];
+
+function mergeSort(arr) {
+    if(arr.length <2) return arr;
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let sorted = [];
+    while (left.length && right.length) {
+        if(left[0]<=right[0]){
+            sorted.push(left.shift());
+        }else{
+            sorted.push(right.shift());
         }
     }
-    return [...quickSort(left),pivot,...quickSort(right)]
+    return [...sorted,...left,...right]
 }
-console.log(quickSort(arr));
+
+console.log(mergeSort(arr));
 
