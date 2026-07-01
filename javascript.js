debe33231fbeb21e89ec4ca7edfc86f0d3ece58d
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-07-01 13:43:53 +0530

    feat : bubblesort standard optimized implementation.

--- javascript.js
+++ javascript.js
@@ -1450,3 +1450,41 @@
 //     }
 // }
 // console.log(firstNonRepeating([1, 5, 7, 1, 5, , 7]));
+
+// const arr = [1,2,3,4,5,6,7,8,9,0,0]
+// const sortAscending = arr.sort((a,b)=>b-a);
+// console.log(sortAscending);
+
+const arr = [5, 3, 8, 4, 2];
+
+function bubbleSort(arr) {
+    const n = arr.length;
+    let swapped;
+    
+    for (let i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+                swapped = true;
+            }
+        }
+        if(!swapped) break;
+    }
+    return arr;
+}
+
+// function bubbleSort(arr) {
+//     let swapped;
+//     do {
+//         swapped = false;
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+//                 swapped = true;
+//             }
+//         }
+//     } while (swapped);
+//     return arr;
+// }
+console.log(bubbleSort(arr));
