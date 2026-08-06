96e8a3928dbfb9e5483161a33067edccc7f3bf12
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-08-06 07:42:44 +0530

    feat:add closure counter example

---
+++ basicpraticals.js
@@ -0,0 +1,12 @@
+function outer(){
+    let count = 0;
+    function inner(){
+        count++;
+        console.log(count);
+    }
+    return inner;
+}
+const counter = outer();
+counter();
+counter();
+counter();
