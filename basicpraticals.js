96a4b0f6935b1d02746ddb4ce7091a19e06f1308
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-08-06 09:37:43 +0530

    feat:add promise creation and consuming example with settimeout event handler

--- basicpraticals.js
+++ basicpraticals.js
@@ -42,11 +42,29 @@
     
 // }
 
-console.log('start');
-setTimeout(() => {
-    console.log('timer');
+// console.log('start');
+// setTimeout(() => {
+//     console.log('timer');
     
-}, 0);
-Promise.resolve().then(()=>console.log('resolved'))
-console.log('end');//First, start goes to the call stack and executes. Then setTimeout() is registered with the browser or Node.js APIs, and its callback waits until the timer finishes. Next, end executes, and the call stack becomes empty. The resolved Promise callback is placed in the microtask queue, which has higher priority than the callback (macrotask) queue. After the timer expires, the setTimeout callback is placed in the callback queue. The event loop checks whether the call stack is empty. It first moves the Promise callback from the microtask queue to the call stack and executes it. After the microtask queue is empty, it moves the setTimeout callback from the callback queue to the call stack and executes it.
+// }, 0);
+// Promise.resolve().then(()=>console.log('resolved'))
+// console.log('end');//First, start goes to the call stack and executes. Then setTimeout() is registered with the browser or Node.js APIs, and its callback waits until the timer finishes. Next, end executes, and the call stack becomes empty. The resolved Promise callback is placed in the microtask queue, which has higher priority than the callback (macrotask) queue. After the timer expires, the setTimeout callback is placed in the callback queue. The event loop checks whether the call stack is empty. It first moves the Promise callback from the microtask queue to the call stack and executes it. After the microtask queue is empty, it moves the setTimeout callback from the callback queue to the call stack and executes it.
 
+const promise = new Promise((res,rej) => {
+    let success = false;
+    if(success){
+        res("ok")
+    }else{
+        rej("not")
+    }
+});
+
+promise.then(re=>console.log(re)).catch(e=>console.log(e)
+);
+
+const user = new Promise((res)=>{
+    setTimeout(() => {
+        res("okey")
+    }, 2000);
+});
+user.then(data => console.log(data))
