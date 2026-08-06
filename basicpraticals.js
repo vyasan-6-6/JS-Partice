af6de53706f26b88cb0835ca5c3618814335967f
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-08-06 07:50:02 +0530

    feat:add closure bankAccount example

--- basicpraticals.js
+++ basicpraticals.js
@@ -1,12 +1,28 @@
-function outer(){
-    let count = 0;
-    function inner(){
-        count++;
-        console.log(count);
+// function outer(){
+//     let count = 0;
+//     function inner(){
+//         count++;
+//         console.log(count);
+//     }
+//     return inner;
+// }
+// const counter = outer();
+// counter();
+// counter();
+// counter();
+
+function bankAccount(){
+    let balance = 2000;
+    return {
+        deposit(amount){
+            balance +=amount
+        }
+   ,     checkBalance (){
+            console.log(balance);
+            
+        }
     }
-    return inner;
 }
-const counter = outer();
-counter();
-counter();
-counter();
+const ac = bankAccount()
+ac.deposit(1000);
+ac.checkBalance();
