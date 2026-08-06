2655420577e468d4e47534f8cfe07a5d153e2910
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-08-06 08:35:24 +0530

    feat:add js hoisting example fn declaration and expression

--- basicpraticals.js
+++ basicpraticals.js
@@ -11,18 +11,33 @@
 // counter();
 // counter();
 
-function bankAccount(){
-    let balance = 2000;
-    return {
-        deposit(amount){
-            balance +=amount
-        }
-   ,     checkBalance (){
-            console.log(balance);
+// function bankAccount(){
+//     let balance = 2000;
+//     return {
+//         deposit(amount){
+//             balance +=amount
+//         }
+//    ,     checkBalance (){
+//             console.log(balance);
             
-        }
-    }
+//         }
+//     }
+// }
+// const ac = bankAccount()
+// ac.deposit(1000);
+// ac.checkBalance();
+
+console.log(x);
+var x ;
+x= 10 ;
+
+hello();//entire fn is hoisted.
+function hello(){
+    console.log("hi");
 }
-const ac = bankAccount()
-ac.deposit(1000);
-ac.checkBalance();
+
+he();// the variable declaration is hoisted , not the assigned fn.
+const he = ()=>{
+    console.log("hey");
+    
+}
