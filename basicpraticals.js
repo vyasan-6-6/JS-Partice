6af2a941bd39cb005de9d26c412d73f9b250108e
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-08-06 09:17:42 +0530

    feat:add callstack execution example and event loop working

--- basicpraticals.js
+++ basicpraticals.js
@@ -27,17 +27,26 @@
 // ac.deposit(1000);
 // ac.checkBalance();
 
-console.log(x);
-var x ;
-x= 10 ;
+// console.log(x);
+// var x ;
+// x= 10 ;
 
-hello();//entire fn is hoisted.
-function hello(){
-    console.log("hi");
-}
+// hello();//entire fn is hoisted.
+// function hello(){
+//     console.log("hi");
+// }
 
-he();// the variable declaration is hoisted , not the assigned fn.
-const he = ()=>{
-    console.log("hey");
+// he();// the variable declaration is hoisted , not the assigned fn.
+// const he = ()=>{
+//     console.log("hey");
     
-}
+// }
+
+console.log('start');
+setTimeout(() => {
+    console.log('timer');
+    
+}, 0);
+Promise.resolve().then(()=>console.log('resolved'))
+console.log('end');//First, start goes to the call stack and executes. Then setTimeout() is registered with the browser or Node.js APIs, and its callback waits until the timer finishes. Next, end executes, and the call stack becomes empty. The resolved Promise callback is placed in the microtask queue, which has higher priority than the callback (macrotask) queue. After the timer expires, the setTimeout callback is placed in the callback queue. The event loop checks whether the call stack is empty. It first moves the Promise callback from the microtask queue to the call stack and executes it. After the microtask queue is empty, it moves the setTimeout callback from the callback queue to the call stack and executes it.
+
